28043dc218cf05a41b90b7728b8bf6a455ae32f9
Danil Galytskyi <85827655+Mackquin@users.noreply.github.com>
2021-06-13 15:30:30 +0300

    Add files via upload

---
+++ TEST.py
@@ -0,0 +1,17 @@
+"""Самописный калькулятор"""
+a = int(input())
+b = int(input())
+print("Уточните операцию + - * /")
+c = input()
+if c == "+":
+    print(a + b)
+elif c == "-":
+    print(a - b)
+elif c == "*":
+    print(a * b)
+elif b == 0 and c == "/":
+    print("На ноль делить нельзя!")
+elif c == "/":
+    print(a / b)
+else:
+    print("Неверная операция")
---
+++ main.py
@@ -0,0 +1,21 @@
+"""рограмма, которая находит вещественные корни квадратного уравнения"""
+import math
+from math import *
+a, b, c = float(input()), float(input()), float(input())
+d = (b ** 2) - (4 * a * c)
+
+x1 = (-b + (d ** 0.5)) / (2 * a)
+x2 = (-b - (d ** 0.5)) / (2 * a)
+
+# если меньше 0
+if d < 0:
+    print("Нет корней")
+
+# если равен 0
+elif d == 0:
+    print(-b / (2 * a))
+
+# если больше 0
+elif d > 0:
+    print(min(x1, x2))
+    print(max(x1, x2))
