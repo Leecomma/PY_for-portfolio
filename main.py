600cc35cf13c01bc87b23bcf6cf025859e67274e
tonykanza <djdanik548@gmail.com>
2024-01-02 01:19:23 +0200

    upd

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-"""программа, которая находит вещественные корни квадратного уравнения"""
 import math
 from math import *
 a, b, c = float(input()), float(input()), float(input())
@@ -7,15 +6,12 @@ d = (b ** 2) - (4 * a * c)
 x1 = (-b + (d ** 0.5)) / (2 * a)
 x2 = (-b - (d ** 0.5)) / (2 * a)
 
-# если меньше 0
 if d < 0:
-    print("Нет корней")
+    print("ZERO")
 
-# если равен 0
 elif d == 0:
     print(-b / (2 * a))
 
-# если больше 0
 elif d > 0:
     print(min(x1, x2))
     print(max(x1, x2))
